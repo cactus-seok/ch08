fe743324e0f7fd1f26476e318cf960b3858e5c7e
cactus-seok <sangpil95@gmail.com>
2021-05-19 20:58:47 +0900

    ch08 예제
    예외처리 예제

---
+++ EnhancedforExample.java
@@ -0,0 +1,12 @@
+public class EnhancedforExample {
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println();
+        int [] k = {0, 1, 2, 3, 4, 5};
+        for(int index : k) {
+            System.out.println(index);
+        }
+    }
+}
---
+++ ExceptionEx1.java
@@ -0,0 +1,7 @@
+class ExceptionEx1 {
+    public static void main(String[] args) {
+        try{
+            try{    } catch(Exception e){}
+        } catch (Exception e){}
+    }
+}
